d3d5f365ae5513abc17402e34b9187ef8dc0d7f7
usaian-jxy <1632442300@qq.com>
2021-04-09 13:57:58 +0800

    商城

---
+++ usian_item_service/src/main/java/com/usian/service/ItemService.java
@@ -0,0 +1,16 @@
+package com.usian.service;
+
+import com.usian.mapper.TbItemMapper;
+import com.usian.pojo.TbItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ItemService {
+    @Autowired
+    private TbItemMapper itemMapper;
+
+    public TbItem selectItemInfo(Long itemId) {
+        return itemMapper.selectByPrimaryKey(itemId);
+    }
+}
